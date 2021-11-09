a77b00b4a02cdeead83881ff6e06a2f191f0bdc7
Quang Nguyen <duongnguyen@rivian.com>
2021-11-08 20:02:20 -0800

    updated Rivian.spec.ts

--- cypress/tests/Rivian.spec.ts
+++ cypress/tests/Rivian.spec.ts
@@ -6,36 +6,36 @@ describe('Rivian', () => {
   it('When I visit R1T page', () => {
     cy.get('[data-testid=nav-link]').contains('R1T').click()
     cy.url().should('include', 'r1t')
-    cy.contains('R1T')
-    cy.contains('A truck built for whatever you call a road. Your electric adventure awaits.')
-    cy.contains('Starting at $67,500 with an EPA estimated range of 314 miles.')
+    for (let text of ['R1T', 'EPA estimated range of 314 miles', 'Starting at $67,500']) {
+      cy.contains(text)
+    }
   })
 
   it('When I visit R1S page', () => {
     cy.get('[data-testid=nav-link]').contains('R1S').click()
     cy.url().should('include', 'r1s')
-    cy.contains('R1S')
-    cy.contains('Explore the places you love with the people you love. Grab your camera. It’s time to make some memories.')
-    cy.contains('Starting at $70,000 with an EPA estimated range of 316 miles.')
+    for (let text of ['R1S', 'EPA estimated range of 316 miles', 'Starting at $70,000']) {
+      cy.contains(text)
+    }
   })
 
-  it('When I visit Experience page', () => {
-    cy.get('[data-testid=nav-link]').contains('Experience').click()
-    cy.url().should('include', 'experience')
-    cy.contains('Our ownership experience is designed to be seamless and intuitive, with benefits focused on the values we share.')
-    cy.contains('Car buying made simple.')
-    cy.contains(
-      'You can manage the entire purchase process on your phone or computer. If you ever have a question, your personal Rivian Guide is here to help. To ensure your happiness, you have 7 days or 1,000 miles to return your vehicle after it’s delivered.¹ Our goal is to make buying a Rivian easy and transparent.'
-    )
+  it('When I visit Gear Shop page', () => {
+    cy.get('[data-testid=nav-link]').contains('Gear Shop').click()
+    cy.url().should('include', 'gear-shop')
+    for (let text of ['All Gear']) {
+      cy.contains(text)
+    }
   })
 
   it('When I visit Fleet page', () => {
     cy.get('[data-testid=nav-link]').contains('Fleet').click()
     cy.url().should('include', 'fleet')
-    cy.contains('Electrify your fleet')
-    cy.contains('Efficient, sustainable solutions for your business and the planet.')
-    cy.contains(
-      'Rivian is developing vehicles, software, charging solutions and services designed to help increase uptime and lower operating costs while helping businesses achieve ambitious sustainability goals.'
-    )
+    for (let text of [
+      'Electrify your fleet',
+      'Efficient, sustainable solutions for your business and the planet.',
+      'Rivian is developing vehicles, software, charging solutions and services designed to help increase uptime and lower operating costs while helping businesses achieve ambitious sustainability goals.',
+    ]) {
+      cy.contains(text)
+    }
   })
 })
